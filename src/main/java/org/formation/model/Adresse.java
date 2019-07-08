7521a0456bb1dc69b304e81713c500482c811fe8
Martial <martial.pouzet@laposte.net>
2019-07-08 16:16:49 +0200

    classe adresse + patient

--- src/main/java/org/formation/model/Adresse.java
+++ src/main/java/org/formation/model/Adresse.java
@@ -1,5 +1,31 @@
 package org.formation.model;
 
 public class Adresse {
+	
+	private Integer numero;
+	private String rue;
+	private String cp;
+	
+	
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public String getRue() {
+		return rue;
+	}
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	public String getCp() {
+		return cp;
+	}
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
+	
+	
 
 }
--- src/main/java/org/formation/model/Patient.java
+++ src/main/java/org/formation/model/Patient.java
@@ -1,5 +1,94 @@
 package org.formation.model;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+@Entity
+@DiscriminatorValue("patient")
 public class Patient {
+	
+	@Enumerated(EnumType.ORDINAL)
+	private Roles role;
+	
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@Temporal(TemporalType.DATE)
+	private Date naissance;
+	
+	private Integer secu;
+	
+	@OneToMany(mappedBy = "id")
+	private List<Rdv> listesRdv;
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naissance == null) ? 0 : naissance.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + ((secu == null) ? 0 : secu.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Patient other = (Patient) obj;
+		if (naissance == null) {
+			if (other.naissance != null)
+				return false;
+		} else if (!naissance.equals(other.naissance))
+			return false;
+		if (role != other.role)
+			return false;
+		if (secu == null) {
+			if (other.secu != null)
+				return false;
+		} else if (!secu.equals(other.secu))
+			return false;
+		return true;
+	}
+
+	public Roles getRole() {
+		return role;
+	}
+
+	public void setRole(Roles role) {
+		this.role = role;
+	}
+
+	public Date getNaissance() {
+		return naissance;
+	}
+
+	public void setNaissance(Date naissance) {
+		this.naissance = naissance;
+	}
+
+	public Integer getSecu() {
+		return secu;
+	}
+
+	public void setSecu(Integer secu) {
+		this.secu = secu;
+	}
+	
+	
 
 }
