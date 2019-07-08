68118205810c7fa2e1c2cb18ef51aa344bd07ff1
selincano <selin.cano@yahoo.com>
2019-07-08 15:45:26 +0200

    RdvPlanning

--- src/main/java/org/formation/model/Planning.java
+++ src/main/java/org/formation/model/Planning.java
@@ -1,5 +1,51 @@
 package org.formation.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Planning {
+	
+	private Praticien praticien;
+	private ArrayList disponibilite;
+	
+	
+public Planning() {
+}
+
+
+public Praticien getPraticien() {
+	return praticien;
+}
+
+
+public void setPraticien(Praticien praticien) {
+	this.praticien = praticien;
+}
+
+
+public ArrayList getDisponibilite() {
+	return disponibilite;
+}
+
+
+public void setDisponibilite(ArrayList disponibilite) {
+	this.disponibilite = disponibilite;
+}
+
+
+public Planning(Praticien praticien, ArrayList disponibilite) {
+	super();
+	this.praticien = praticien;
+	this.disponibilite = disponibilite;
+}
+
+
+@Override
+public String toString() {
+	return "Planning [praticien=" + praticien + ", disponibilite=" + disponibilite + "]";
+}
+
+
+
 
 }
--- src/main/java/org/formation/model/Rdv.java
+++ src/main/java/org/formation/model/Rdv.java
@@ -1,5 +1,96 @@
 package org.formation.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import org.springframework.data.annotation.Id;
+
+@Entity
 public class Rdv {
+	@Id
+	private Integer id;
+	@OneToMany
+	private Patient patients;
+	@OneToMany
+	private Praticien praticien;
+	@OneToMany
+	private Planning date;
+	private String motif;
+	private String lieu;
+	
+	public Rdv() {	
+	}
+	
+
+	public Rdv(Integer id, Patient patients, Praticien praticien, Planning date, String motif, String lieu) {
+		super();
+		this.id = id;
+		this.patients = patients;
+		this.praticien = praticien;
+		this.date = date;
+		this.motif = motif;
+		this.lieu = lieu;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Patient getPatients() {
+		return patients;
+	}
+
+	public void setPatients(Patient patients) {
+		this.patients = patients;
+	}
+
+	public Praticien getPraticien() {
+		return praticien;
+	}
+
+	public void setPraticien(Praticien praticien) {
+		this.praticien = praticien;
+	}
+
+	public Planning getDate() {
+		return date;
+	}
+
+	public void setDate(Planning date) {
+		this.date = date;
+	}
+
+	public String getMotif() {
+		return motif;
+	}
+
+	public void setMotif(String motif) {
+		this.motif = motif;
+	}
+
+	public String getLieu() {
+		return lieu;
+	}
+
+	public void setLieu(String lieu) {
+		this.lieu = lieu;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Rdv [id=" + id + ", patients=" + patients + ", praticien=" + praticien + ", date=" + date + ", motif="
+				+ motif + ", lieu=" + lieu + "]";
+	}
+	
+
+
 
 }
