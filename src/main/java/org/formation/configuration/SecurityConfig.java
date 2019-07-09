e13e04fea2e3768dc7f97bfca6dd62a9380bb885
gegeli <monlouiselisee@gmail.com>
2019-07-09 10:35:43 +0200

    myUserDetail, User, Secuconfig

---
+++ src/main/java/org/formation/configuration/SecurityConfig.java
@@ -0,0 +1,8 @@
+package org.formation.configuration;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+	
+}
---
+++ src/main/java/org/formation/service/MyUserDetails.java
@@ -0,0 +1,10 @@
+package org.formation.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetails implements UserDetails
+{
+
+}
