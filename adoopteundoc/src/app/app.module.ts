51107c522a5a6d22b9b2f93f8803dc59a4abfa3a
Raphaelfosse <raphael.fosse91@gmail.com>
2019-07-10 11:21:07 +0200

    model fixed

--- adoopteundoc/src/app/app.module.ts
+++ adoopteundoc/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PraticienComponent } from './praticien/praticien.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PraticienComponent
   ],
   imports: [
     BrowserModule
